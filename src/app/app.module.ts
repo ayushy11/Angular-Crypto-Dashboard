65220f5866a2ebe7a5118a9b71302a6f458921d9
AYUSH YADAV <ayushexx@gmail.com>
2022-05-09 18:27:36 +0530

    api service & marquee rows

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,6 +20,7 @@ import { CoinDetailComponent } from './coin-detail/coin-detail.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatToolbarModule,
     MatSelectModule,
     MatFormFieldModule,
--- src/app/coin-list/coin-list.component.ts
+++ src/app/coin-list/coin-list.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
 
 @Component({
   selector: 'app-coin-list',
   templateUrl: './coin-list.component.html',
-  styleUrls: ['./coin-list.component.scss']
+  styleUrls: ['./coin-list.component.scss'],
 })
 export class CoinListComponent implements OnInit {
-
-  constructor() { }
+  bannerData: any = [];
+  allData: any = [];
+  constructor(private api: ApiService) {}
 
   ngOnInit(): void {
+    this.getAllData();
+    this.getBannerData();
   }
 
+  getBannerData() {
+    this.api
+      .getTrendingCurrency('INR')
+      .subscribe((data) => (this.bannerData = data));
+  }
+
+  getAllData() {
+    this.api.getCurrency('INR').subscribe((data) => (this.allData = data));
+  }
 }
